360ded41c84872fe7ff4fbe0a98abee4f875ea3a
Jakub Wąsala <jakubwasala1@gmail.com>
2021-12-01 17:23:32 +0100

    Implemented getWindows and countIncreases functions and used them to solve task 1 and task 2 from day 1

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -13,14 +13,29 @@ object Day01 extends App {
         .toList
     ).get
 
-  def countIncreases(xs: List[Int]): Int =
-    xs.tail
-      .zip(xs)
+  def getWindows(xs: List[Int], windowSize: Int): List[List[Int]] =
+    xs
+      .drop(windowSize)
+      .foldLeft(List(xs.take(windowSize)))((acc, x) => {
+        (acc.head.tail ::: List(x)) :: acc
+      })
+      .reverse
+
+  def countIncreases(xs: List[Int], windowSize: Int): Int = {
+    val windows: List[List[Int]] = getWindows(xs, windowSize)
+
+    windows.tail
+      .zip(windows)
       .foldLeft(0)((acc, zipped) => {
-        if (zipped._1 > zipped._2) acc + 1
+        if (zipped._1.sum > zipped._2.sum) acc + 1
         else acc
       })
+  }
 
+  val input: List[Int] = readLinesFromResource("Day01.input.txt")
   println("Task 1 solution:")
-  println(countIncreases(readLinesFromResource("Day01.input.txt")))
+  println(countIncreases(input, 1))
+  println("Task 2 solution:")
+  println(countIncreases(input, 3))
+
 }
